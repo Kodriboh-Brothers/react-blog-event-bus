21e302b4158f48744cf2b973d241543022f75ceb
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-11 19:49:56 +0000

    Add basic event bus

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = require('express')();
+app.use(bodyParser.json());
+
+const port = process.env.EVENT_BUS_PORT || 4005;
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+
+    res.send({ status: 'OK' });
+});
+
+app.listen(port, () => {
+    console.log(`Event Bus listening on port: ${port}`);
+});
